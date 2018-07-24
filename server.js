b88be642e7b9bba896da68283423f07860890757
Glitch (expobot) <none>
2018-07-24 10:50:39 +0000

    🌨📇 Checkpoint ./server.js:1424713/321 ./about/index.html:1424713/62 ./commands/index.html:1424713/5850

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -26,7 +17,6 @@ app.get('/about', function(request, response) {
 
 app.get('*', function(req, res){ res.status(404).sendFile(__dirname + '/404/index.html'); });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
