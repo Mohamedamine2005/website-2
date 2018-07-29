d2b438b971289078e7f90adcb196cb512759636f
Glitch (expobot) <none>
2018-07-29 09:52:49 +0000

    🛬🍍 Checkpoint ./server.js:1424713/3735

--- server.js
+++ server.js
@@ -12,26 +12,52 @@ app.get('/', function(request, response) {
 
 /*START AUTH*/
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
+
 passport.use(new DiscordStrategy({
-    clientID: 'id',
-    clientSecret: 'secret',
-    callbackURL: 'callbackURL'
-},
-function(accessToken, refreshToken, profile, cb) {
-    if (err)
-        return done(err);
- 
-    User.findOrCreate({ discordId: profile.id }, function(err, user) {
-        return cb(err, user);
+    clientID: '',
+    clientSecret: '',
+    callbackURL: 'http://localhost:5000/callback',
+    scope: scopes
+}, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+        return done(null, profile);
     });
 }));
 
-app.get('/auth/discord', passport.authenticate('discord'));
-app.get('/auth/discord/callback', passport.authenticate('discord', {
-    failureRedirect: '/'
-}), function(req, res) {
-    res.redirect('/secretstuff') // Successful auth
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/', passport.authenticate('discord', { scope: scopes }), function(req, res) {});
+app.get('/callback',
+    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { res.redirect('/info') } // auth success
+);
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
+app.get('/info', checkAuth, function(req, res) {
+    //console.log(req.user)
+    res.json(req.user);
+});
+
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.send('not logged in :(');
+}
+
 
 /*END OF AUTH*/
 
