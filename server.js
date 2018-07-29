e0f5057863fe603d6b97ccac0e1e889bd7dfe843
Glitch (expobot) <none>
2018-07-29 10:35:19 +0000

    💁🎱 Checkpoint ./strategy.js:1424713/4771 ./server.js:1424713/1350 ./router.js:1424713/73

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var app = express();
-var session  = require('express-session');
-var passport = require('passport');
-var DiscordStrategy = require("passport-discord").Strategy;
 
 app.use(express.static('public'));
 
@@ -12,45 +9,7 @@ app.get('/', function(request, response) {
 
 /*START AUTH*/
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-var scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
-
-passport.use(new DiscordStrategy({
-    clientID: '464747957288435732',
-    clientSecret: 'BwerPCx896WSIY_uQhfgBgZj4l5GXir1',
-    scope: scopes
-}, function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function() {
-        return done(null, profile);
-    });
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.get('/auth', passport.authenticate('discord', { scope: scopes }), function(req, res) {});
-app.get('/callback',
-    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { res.redirect('/info') } // auth success
-);
-app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-});
-app.get('/info', checkAuth, function(req, res) {
-    console.log(req.user)
-    res.json(req.user);
-});
-
-
-function checkAuth(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.send('not logged in :(');
-}
+app.use('/api/discord', require('./router'));
 
 
 /*END OF AUTH*/
