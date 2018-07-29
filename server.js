6727e67d5571a0d1a3e7a425eb9edf40748a0d17
Glitch (expobot) <none>
2018-07-29 09:42:06 +0000

    👐🎽 Checkpoint ./server.js:1424713/4329

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var session  = require('express-session')
 var passport = require('passport');
-var Strategy = require('passport-discord').Strategy;
+var DiscordStrategy = require('passport-discord').Strategy;
 
 app.use(express.static('public'));
 
@@ -11,50 +11,28 @@ app.get('/', function(request, response) {
 });
 
 /*START AUTH*/
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
 
-var scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
-
-passport.use(new Strategy({
-    clientID: '464747957288435732',
-    clientSecret: 'BwerPCx896WSIY_uQhfgBgZj4l5GXir1',
-    callbackURL: 'https://expobot.glitch.me/',
-    scope: scopes
-}, function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function() {
-        return done(null, profile);
+passport.use(new DiscordStrategy({
+    clientID: 'id',
+    clientSecret: 'secret',
+    callbackURL: 'callbackURL'
+},
+function(accessToken, refreshToken, profile, cb) {
+    if (err)
+        return done(err);
+ 
+    User.findOrCreate({ discordId: profile.id }, function(err, user) {
+        return cb(err, user);
     });
 }));
 
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.get('/login', passport.authenticate('discord', { scope: scopes }), function(req, res) {});
-app.get('/callback',
-    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { res.redirect('/commands/index.html') } // auth success
-);
-app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/about/index.html');
-});
-app.get('/info', checkAuth, function(req, res) {
-    //console.log(req.user)
-    res.json(req.user);
+app.get('/auth/discord', passport.authenticate('discord'));
+app.get('/auth/discord/callback', passport.authenticate('discord', {
+    failureRedirect: '/'
+}), function(req, res) {
+    res.redirect('/secretstuff') // Successful auth
 });
 
-function checkAuth(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.send('not logged in :(');
-}
 /*END OF AUTH*/
 
 app.get('/commands', function(request, response) {
