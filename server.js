ac4cae9554ee3a293382c1373b171456fe39cac4
Glitch (expobot) <none>
2018-07-31 17:52:38 +0000

    👩🍻 Checkpoint ./router.js:1424713/104 ./views/index.html:1424713/534 ./server.js:1424713/381

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-/*START AUTH*/
-
-app.use('/login', require('./router'));
-
-/*END OF AUTH*/
-
 app.get('/commands', function(request, response) {
   response.sendFile(__dirname + '/commands/index.html');
 });
@@ -25,19 +19,4 @@ app.get('*', function(req, res){ res.status(404).sendFile(__dirname + '/404/inde
 
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
-});
-
-app.use((err, req, res, next) => {
-  switch (err.message) {
-    case 'NoCodeProvided':
-      return res.status(400).send({
-        status: 'ERROR',
-        error: err.message,
-      });
-    default:
-      return res.status(500).send({
-        status: 'ERROR',
-        error: err.message,
-      });
-  }
 });
