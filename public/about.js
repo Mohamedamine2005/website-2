a6b1226d7a1dd1a2c7b5c611f4e599495070c7df
Glitch (expobot) <none>
2018-08-01 17:02:43 +0000

    🏫☂️ Checkpoint ./public/about.js:1424713/374 ./about/index.html:1424713/25

--- public/about.js
+++ public/about.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
 
   $('.sidenav').sidenav();
   
-   // Init ScrollMagic
-   var ctrl = new ScrollMagic.Controller();
+  // Init ScrollMagic
+  var ctrl = new ScrollMagic.Controller();
+   
+  //$('.opacity').each(function () {
     
-   ${'.opacity'}
+    var Scene = new ScrollMagic.Scene({
+      triggerElement: "half",
+      triggerHook: 0.9
+    })
+    .setClassToggle("opacity", "fadeIn")
+    .addTo(ctrl);
+    
+ // });
 
 });
