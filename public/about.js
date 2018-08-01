01bafabd0a913004f6342989aa2dd3e1f023fb1e
Glitch (expobot) <none>
2018-08-01 16:39:19 +0000

    🍭💄 Checkpoint ./public/about.js:1424713/467 ./about/index.html:1424713/479

--- public/about.js
+++ public/about.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
 
   $('.sidenav').sidenav();
   
-  
+   // Init ScrollMagic
+   var ctrl = new ScrollMagic.Controller();
+    
+   var half1 = new ScrollMagic.Scene({
+     triggerElement: ".half",
+     triggerHook: 1,
+     duration: "100%"
+   })
+   .setTween(TweenMax
+     .from('.half1', 1, {
+       x: "100%",
+       ease: Strong.easeOut
+     })
+   )
+   .addTo(ctrl);
 
 });
