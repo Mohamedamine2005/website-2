c9de9f72c6eb507555f3da41375692d0a0e7ab8f
Glitch (expobot) <none>
2018-08-13 12:34:10 +0000

    🐺💸 Checkpoint ./server.js:1424713/107 ./views/index.html:1424713/7

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ app.get('/commands', function(request, response) {
   response.sendFile(__dirname + '/commands/index.html');
 });
 
-app.get('/about', function(request, response) {
-  response.sendFile(__dirname + '/about/index.html');
-});
-
 app.get('*', function(req, res){ res.status(404).sendFile(__dirname + '/404/index.html'); });
 
 var listener = app.listen(process.env.PORT, function() {
