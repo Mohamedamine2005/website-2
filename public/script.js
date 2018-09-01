d63d0c8fed4ba2be8caf1d667f2988f627f49292
Glitch (expobot) <none>
2018-09-01 11:51:14 +0000

    😺🦃 Checkpoint ./views/index.html:1424713/54 ./public/script.js:1424713/17

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.sidenav').sidenav();
   $('.tabs').tabs();
   
-  var myFullpage = new fullpage('.fullpage', {
+  new fullpage('.fullpage', {
     sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
   });
 
