34f594bf51b53ff1076ecb74ab58e567f977a549
Glitch (expobot) <none>
2018-09-01 09:56:31 +0000

    🌮👮 Checkpoint ./views/index.html:1424713/1847 ./public/materialize.css:1424713/4 ./public/script.js:1424713/102

--- public/script.js
+++ public/script.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
 
   $('.sidenav').sidenav();
   $('.tabs').tabs();
+  
+  new fullpage('#fullpage', {
+    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+  });
 
 });
