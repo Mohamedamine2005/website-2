3631a1d559bac5c9293f372a72e24a79bd2d8329
Glitch (expobot) <none>
2018-09-01 12:24:35 +0000

    📖🎤 Checkpoint ./views/index.html:1424713/361 ./public/script.js:1424713/367

--- public/script.js
+++ public/script.js
@@ -5,7 +5,13 @@ $(document).ready(function() {
   
   new fullpage('.fullpage', {
     navigation: true,
-    sectionsColor: ['transparent', '#fff', '#111']
+    sectionsColor: ['transparent', '#fff', '#111'],
+    loopBottom: true,
+    dragAndMove: true
   });
+  
+  if ($('body').hasClass('fp-viewing-0')) {console.log("1")}
+  if ($('body').hasClass('fp-viewing-1')) {console.log("2")}
+  if ($('body').hasClass('fp-viewing-2')) {console.log("3")}
 
 });
