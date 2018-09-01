7839ac9696b8f181275ee57a5ce2b52d9ca7f863
Glitch (expobot) <none>
2018-09-01 12:45:42 +0000

    🎖🌏 Checkpoint ./public/script.js:1424713/1342 ./package.json:1424713/1595 ./watch.json:1424713/317 ./views/index.html:1424713/38 ./public/style.css:1424713/4

--- public/script.js
+++ public/script.js
@@ -4,25 +4,9 @@ $(document).ready(function() {
   $('.tabs').tabs();
   
   new fullpage('.fullpage', {
+    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
     navigation: true,
-    loopBottom: true,
-    dragAndMove: true
+    loopBottom: true
   });
-  
-  $( window ).scroll(function() {
-    var scroll = window.pageYOffset || document.documentElement.scrollTop;
- 
-    if (scroll == 0) {
-      $('nav').removeClass('blackFade');
-      $('nav').addClass('transparent');
-    } else {
-      $('nav').removeClass('transparent');
-      $('nav').addClass('blackFade');
-    }
-  });
-  
-  if ($('body').hasClass('fp-viewing-0')) {console.log("1")}
-  if ($('body').hasClass('fp-viewing-1')) {console.log("2")}
-  if ($('body').hasClass('fp-viewing-2')) {console.log("3")}
 
 });
