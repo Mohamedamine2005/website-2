002e201bbffb1a1ba817c6003492a2c2e9221228
Glitch (expobot) <none>
2018-09-01 10:06:49 +0000

    🌎🗺 Checkpoint ./public/style.css:1424713/8 ./public/script.js:1424713/44 ./views/index.html:1424713/13693

--- public/script.js
+++ public/script.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
   
   new fullpage('#fullpage', {
     sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+    navigation: true,
+    paralax: true
   });
 
 });
