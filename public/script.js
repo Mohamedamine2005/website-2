2a1e5b19b910145ca15cba6adaa492aa73b65fc9
Glitch (expobot) <none>
2018-09-22 18:26:00 +0000

    🚒🐠 Checkpoint ./public/script.js:1424713/822

--- public/script.js
+++ public/script.js
@@ -10,5 +10,16 @@ $(document).ready(function() {
     loopBottom: true,
     sectionsColor: ['transparent', '#fff', '#111']
   });
+  
+  $(window).load(function() {
+    if ($(".podmena1").val().split(" ")[1] != "8 (812)") {
+      var res = $(".podmena1").html().split(" ");
+      $(".podmena1").html("8 (812) " + res[2]);
+      $("#tel").attr('href', "tel:8812" + res[2].split("-")[0] + res[2].split("-")[1] + res[2].split("-")[2]);
+      $("#tel_popup").attr('href', "tel:8812" + res[2].split("-")[0] + res[2].split("-")[1] + res[2].split("-")[2]);
+      $("#tel2").attr('href', "tel:8812" + res[2].split("-")[0] + res[2].split("-")[1] + res[2].split("-")[2]);
+      $("#tel3").attr('href', "tel:8812" + res[2].split("-")[0] + res[2].split("-")[1] + res[2].split("-")[2]);
+    }
+  });
 
 });
